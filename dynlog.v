83e8535d7d2dc07a50cd530a4ddb6b2e67d67de0
Artyom Ivanov <artem_appstore@mail.ru>
2017-10-21 18:03:22 +0300

    Started fucntor

--- dynlog.v
+++ dynlog.v
@@ -5,3 +5,17 @@ Module Type DYNLOGIC.
   Parameters program state : Set.
   Parameter meanFunc : program -> state * state.
 End DYNLOGIC.
+
+Module PDL (D : DYNLOGIC).
+ Inductive prog : Type :=
+ | Elem : D.program -> prog
+ | Sequence : prog -> prog -> prog
+ | Choice : prog -> prog -> prog
+ | Iteration : prog -> prog
+ | Test : Prop -> prog.
+
+ Definition Assertion := D.state -> Prop.
+ Definition box (A : Assertion) : Prop := forall (st : D.state), A st.
+ Definition diamond (A : Assertion) : Prop := exists (st : D.state), A st.
+
+End PDL.
