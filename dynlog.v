07cd8bf75624859f8f839a932229c06813fcc453
Artyom Ivanov <artem_appstore@mail.ru>
2017-10-22 09:40:26 +0300

    Making fucntor

--- dynlog.v
+++ dynlog.v
@@ -1,22 +1,32 @@
 Require Import Notations.
 Require Import Logic.
 
+Definition relation (X: Type) := X -> X -> Prop.
+
 Module Type DYNLOGIC.
-  Parameters program state : Set.
-  Parameter meanFunc : program -> state * state.
+  Parameters atomic_prog state : Set.
+  Parameter meanFunc : atomic_prog -> relation state.
 End DYNLOGIC.
 
-Module PDL (D : DYNLOGIC).
+Module PDL (Import D : DYNLOGIC).
+ Definition Assertion := state -> Prop.
+
+ Definition impl (A B : Assertion) : Assertion := (fun x => (A x -> B x)).
+ Definition necess (A : Assertion) : Prop := forall (st : state), A st.
+ Definition possib (A : Assertion) : Prop := exists (st : state), A st.
+
  Inductive prog : Type :=
- | Elem : D.program -> prog
+ | Elem : atomic_prog -> prog
  | Sequence : prog -> prog -> prog
  | Choice : prog -> prog -> prog
  | Iteration : prog -> prog
- | Test : Prop -> prog.
+ | Test : Assertion -> prog.
+ 
+ Definition box (ap : atomic_prog) (A : Assertion) : Prop := 
+   forall (st st' : state), meanFunc ap st st' -> necess A.
 
- Definition Assertion := D.state -> Prop.
- Definition impl (A B : Assertion) : Assertion := (fun x => (A x -> B x)).
- Definition necess (A : Assertion) : Prop := forall (st : D.state), A st.
- Definition possib (A : Assertion) : Prop := exists (st : D.state), A st.
+
+ 
+ 
  
 End PDL.
