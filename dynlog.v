88e3786a1812b4968b725a2a808dc45a73fa5631
Artyom Ivanov <artem_appstore@mail.ru>
2017-10-24 09:59:41 +0300

    Making fucntor

--- dynlog.v
+++ dynlog.v
@@ -1,42 +1,49 @@
 Require Import Notations.
 Require Import Logic.
-Require Import Relations.Relation_Operators.
+Require Import Relations.
+
 Arguments clos_refl_trans_1n {A} R x _.
 Arguments clos_trans {A} R x _.
 Arguments union {A} R1 R2 x y.
 
-Definition relation (X: Type) := X -> X -> Prop.
+Module Type DynLogic.
+  Parameters atomProg state : Set.
+  Parameter meanFunc : atomProg -> relation state.
+End DynLogic.
 
-Module Type DYNLOGIC.
-  Parameters atomic_prog state : Set.
-  Parameter meanFunc : atomic_prog -> relation state.
-End DYNLOGIC.
+Section Compose.
+  Variable T : Type.
+  Variables R1 R2 : relation T.
+  Inductive composeRelSt : relation T :=
+  | comp_intro (x y z : T) : R1 x y -> R2 y z -> composeRelSt x z.
+End Compose.
+Arguments composeRelSt {T} R1 R2 _ _.
 
-Module PDL (Import D : DYNLOGIC).
- Definition Assertion := state -> Prop.
+Module PDL (Import D : DynLogic).
+ Definition assertion := state -> Prop.
 
- Definition impl (A B : Assertion) : Assertion := (fun x => (A x -> B x)).
- Definition necess (A : Assertion) : Prop := forall (st : state), A st.
- Definition possib (A : Assertion) : Prop := exists (st : state), A st.
-
+ Definition impl (A B : assertion) : assertion := (fun x => (A x -> B x)).
+ 
  Inductive prog : Type :=
- | Elem : atomic_prog -> prog
+ | Elem : atomProg -> prog
  | Sequence : prog -> prog -> prog
  | Choice : prog -> prog -> prog
  | Iteration : prog -> prog
- | Test : Assertion -> prog.
+ | Test : assertion -> prog.
  
-Fixpoint progSemantics  (p : prog) : relation state :=
+ Fixpoint progSemantics  (p : prog) : relation state :=
  match p with
  | Elem ap => meanFunc ap
- | Sequence p1 p2 => clos_trans (progSemantics p1)
+ | Sequence p1 p2 => composeRelSt (progSemantics p1) (progSemantics p2)
  | Choice p1 p2 => union (progSemantics p1) (progSemantics p2) 
  | Iteration p => clos_refl_trans_1n (progSemantics p) 
  | Test A => eq
  end.
   
- Definition box (pr : prog) (A : Assertion) : Prop := 
-   forall (st st' : state), progSemantics  pr st st' -> necess A.
+ Definition box (pr : prog) (A : assertion) : assertion := 
+   fun st' => (forall st, progSemantics pr st st' -> A st').
+ 
+ Check box.
 
 
  
